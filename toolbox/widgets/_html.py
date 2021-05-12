fa40792621ca3d8a64ef216a53017d076be3140d
JollyRodger98 <twaldesbuehl@gmail.com>
2021-05-12 15:31:56 +0200

    HTML template file created

---
+++ toolbox/widgets/_html.py
@@ -0,0 +1,29 @@
+from jinja2 import Markup
+
+table_template: Markup = Markup(
+    f"<div class=\"table-responsive\">\n"
+    f"    <table class=\"table table-sm\">\n"
+    f"        <thead>\n"
+    f"            %(head)s\n"
+    f"        </thead>\n"
+    f"        <tbody>\n"
+    f"            %(body)s\n"
+    f"        </tbody>\n"
+    f"    </table>\n"
+    f"</div>\n")
+table_row_template: Markup = Markup("<tr>%s</tr>")
+table_col_head_template: Markup = Markup("<th scope=\"col\">%s</th>")
+table_row_head_template: Markup = Markup("<th scope=\"row\">%s</th>")
+table_data_template: Markup = Markup("<td>%s</td>")
+widget_card_template: Markup = Markup(
+    f"<div class=\"col\">\n"
+    f"    <div class=\"card user-select-none h-100\">\n"
+    f"        <div class=\"card-body\">\n"
+    f"            <div class=\"card-subtitle text-muted mb-2\">%(card_title)s</div>\n"
+    f"            <div class=\"card-text\">\n"
+    f"                %(card_content)s\n"
+    f"            </div>\n"
+    f"        </div>\n"
+    f"    </div>\n"
+    f"</div>\n"
+)
