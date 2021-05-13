c00d823cc75c37d7d458906751008ad6f663dcb7
Tim Waldesbühl <twaldesbuehl@gmail.com>
2021-05-13 23:44:02 +0200

    Generate each widget as Markup string.

--- toolbox/widgets/dashboard.py
+++ toolbox/widgets/dashboard.py
@@ -2,14 +2,59 @@ from collections import OrderedDict
 from toolbox.hardware import HardwareInfo
 from toolbox.models import DashboardProfiles, widget_list
 from jinja2 import Markup
-from typing import Union, Final
-from ._html import table_template, widget_card_template, table_col_head_template, table_row_head_template,\
-    table_data_template, table_row_template, alert_danger_template
+from typing import Union, Final, Any
+from ._html import table_template, widget_card_template, table_col_head_template, table_row_head_template, \
+    table_data_template, table_row_template, alert_danger_template, table_data_percentage_template
+
+
+def _get_profile(profile: str, widget: str) -> DashboardProfiles:
+    """Query profile settings for widget.
+
+    :param profile: Dashboard profile name
+    :param widget: Name of widget profile is queried for.
+    :return: MongoEngine document with widget settings for profile.
+    :rtype: DashboardProfiles
+    """
+    return DashboardProfiles.objects(profile_name=profile).only(f"widgets.{widget}").first()
+
+
+def _widget_visible(display_setting: bool) -> str:
+    """Get required CSS class for widget display settings.
+
+    :param display_setting:
+    :return: CSS visibility class
+    :rtype: str
+    """
+    if not display_setting:
+        return "visually-hidden"
+    elif display_setting:
+        return ""
+
+
+def _percentage_bar_class(percentage: Union[int, float]) -> str:
+    """Get bar color based on percentage value.
+
+    :param percentage: Percentage bar value
+    :return: CSS color class
+    :rtype: str
+    """
+    minor_threshold = 50
+    major_threshold = 75
+    if minor_threshold > percentage > 0:
+        return "bg-success"
+    elif major_threshold > percentage > minor_threshold:
+        return "bg-warning"
+    elif 100 > percentage > major_threshold:
+        return "bg-danger"
 
 
 class HardwareWidget(HardwareInfo):
 
     def __init__(self):
+        """Generate hardware specification widgets.
+
+        Uses HardwareInfo class to query local hardware specs. Generates Markup string with widget and spec table.
+        """
         super().__init__()
         self.widget_list: list = widget_list()
         self._table: Final[Markup] = table_template
@@ -19,6 +64,9 @@ class HardwareWidget(HardwareInfo):
         self._table_data: Final[Markup] = table_data_template
         self._table_row: Final[Markup] = table_row_template
         self._alert: Final[Markup] = alert_danger_template
+        self._table_data_percentage: Final[Markup] = table_data_percentage_template
+        self._std_table_header: Final[Markup] = \
+            self._table_row % {"row_class": "", "row_content": (self._table_col_head % '' * 2)}
 
     def get_widget(self, widget_name: str, profile: str = "Default") -> OrderedDict:
         if widget_name.lower() not in self.widget_list:
@@ -31,35 +79,165 @@ class HardwareWidget(HardwareInfo):
         return widget_data
 
     def get_widget_html(self, widget_name: str, profile: str = "Default") -> Markup:
-        widget: Markup
+        """Get widget HTML Markup string with name and profile.
 
+        :param widget_name: Name of the widget.
+        :param profile: Name of the dashboard profile.
+        :return: Markup string with entire widget card
+        :rtype: Markup
+        """
         if widget_name.lower() == "memory":
-            widget = self._memory_widget()
+            widget = self._memory_widget(profile)
+        elif widget_name.lower() == "system":
+            widget = self._system_widget(profile)
+        elif widget_name.lower() == "cpu":
+            widget = self._cpu_widget(profile)
+        elif widget_name.lower() == "partitions":
+            widget = self._partitions_widget(profile)
         else:
             li_widget_list = [f"<li>{i}</li>" for i in self.widget_list]
             ul_widget_list = Markup("<ul>%s</ul>") % Markup(''.join(li_widget_list))
             alert_content = Markup(f"<p>Available widgets are:<p>\n{ul_widget_list}")
             card_content = self._alert % {"alert_title": f"Widget '{widget_name}' not found.",
                                           "alert_body": alert_content}
-            widget = self._widget_card % {"card_title": "ERROR", "card_content": card_content}
+            widget = self._widget_card % {"card_title": "ERROR", "card_content": card_content, "card_classes": ""}
 
         return widget
 
-    def _memory_widget(self) -> Markup:
-        """Generate memory widget and return HTML Markup string.
+    def _partitions_widget(self, profile: str) -> Markup:
+        table_data: list = list()
+        # noinspection PyPep8Naming
+        WIDGET = "partitions"
+        user_profile = _get_profile(profile, WIDGET)
+        card_classes = [_widget_visible(user_profile.widgets.partitions.display_widget)]
+
+        for partition in self.get_partitions()["data_list"]:
+            for data in partition.items():
+                print(data)
+                row_content = self._table_row_head % data[0].title() + self._table_data % {
+                    "cell_class": "", "cell_content": data[1]}
+                table_data.append(
+                    self._generate_table_row(data[0], user_profile.widgets.partitions.display_fields, row_content))
+
+        return self._generate_std_widget(WIDGET.title(), table_data, card_classes)
+
+    def _memory_widget(self, profile: str) -> Markup:
+        """Generate memory widget Markup string from hardware data.
 
         :return: HTML Markup string with widget
         :rtype: Markup
         """
         table_data: list = list()
-        table_header = self._table_row % (self._table_col_head % '' * 2)
+        # noinspection PyPep8Naming
+        WIDGET = "memory"
+        user_profile = _get_profile(profile, WIDGET)
+        card_classes = [_widget_visible(user_profile.widgets.memory.display_widget)]
 
         for data in self.get_memory().items():
-            row_content = self._table_row_head % data[0].title() + self._table_data % data[1]
-            table_data.append(self._table_row % row_content)
+            if data[0].lower() == "percent":
+                row_content = self._generate_percentage_row(data[0].title(), data[1])
+            else:
+                row_content = self._table_row_head % data[0].title() + \
+                              self._table_data % {"cell_class": "", "cell_content": data[1]}
+
+            table_data.append(
+                self._generate_table_row(data[0], user_profile.widgets.memory.display_fields, row_content))
+
+        return self._generate_std_widget(WIDGET.title(), table_data, card_classes)
+
+    def _system_widget(self, profile: str) -> Markup:
+        table_data: list = list()
+        # noinspection PyPep8Naming
+        WIDGET = "system"
+        user_profile = _get_profile(profile, WIDGET)
+        card_classes = [_widget_visible(user_profile.widgets.system.display_widget)]
+
+        for data in self.get_system().items():
+            row_content = self._table_row_head % data[0].title() + \
+                          self._table_data % {"cell_class": "", "cell_content": data[1]}
+            table_data.append(
+                self._generate_table_row(data[0], user_profile.widgets.system.display_fields, row_content))
+
+        return self._generate_std_widget(WIDGET.title(), table_data, card_classes)
+
+    def _cpu_widget(self, profile: str) -> Markup:
+        table_data: list = list()
+        # noinspection PyPep8Naming
+        WIDGET = "cpu"
+        user_profile = _get_profile(profile, WIDGET)
+        card_classes = [_widget_visible(user_profile.widgets.cpu.display_widget)]
+
+        for data in self.get_cpu().items():
+            field_name = data[0].title().replace("Cpu", "CPU") if "cpu" in data[0].lower() else data[0].title()
+            alignment = ""
+
+            if data[0].lower() == "cpu usage by core":
+                cores = []
+                for index, field in enumerate(data[1]):
+                    percentage = int(float(data[1][field].replace('%', '')))
+                    cores.append(f"<span class=\"text-nowrap\"><strong>{field+1:02d}</strong>:{percentage:02d}%;</span>")
+                    if (index+1) % 4 == 0:
+                        cores.append(" ")
+
+                row_data = Markup("".join(cores))
+                alignment = "align-middle"
+                row_content = self._table_row_head % field_name + self._table_data % {
+                    "cell_class": "font-TBMonospace h6", "cell_content": row_data}
+            elif data[0].lower() == "cpu usage total":
+                row_content = self._generate_percentage_row(field_name, data[1])
+            else:
+                row_content = self._table_row_head % data[0].title() + \
+                              self._table_data % {"cell_class": "", "cell_content": data[1]}
+
+            table_data.append(
+                self._generate_table_row(data[0], user_profile.widgets.cpu.display_fields, row_content, [alignment]))
+
+        return self._generate_std_widget(WIDGET.upper(), table_data, card_classes)
+
+    def _generate_percentage_row(self, key: str, value: str) -> Markup:
+        """Generate table row content with percentage-bar
+
+        :param key: Field name
+        :param value: Field value (percentage)
+        :return: Table row content (th & td)
+        """
+        percentage = float(value.replace('%', ''))
+        percentage_class = _percentage_bar_class(percentage)
+        row_head = self._table_row_head % key
+        row_data = self._table_data_percentage % {"percentage": percentage, "bar_class": percentage_class}
+        return row_head + row_data
+
+    def _generate_std_widget(self, card_title: str, table_data: list, card_classes: list) -> Markup:
+        """Takes Markup strings and generates a full widget.
+
+        Table data and card classes are joined to string.
+
+        :param card_title: Title to be displayed at the top of the card.
+        :param table_data: Content of specs table as list.
+        :param card_classes: Classes to be applied to the entire card.
+        :return: Widget as Markup string
+        """
+        card_classes = " ".join(card_classes)
         table_data = Markup("\n".join(table_data))
+        table = self._table % {"head": self._std_table_header, "body": table_data}
+        return self._widget_card % {"card_title": card_title, "card_content": table, "card_classes": card_classes}
 
-        table = self._table % {"head": table_header, "body": table_data}
-        widget = self._widget_card % {"card_title": "Memory", "card_content": table}
+    def _generate_table_row(self, field_name: str, display_settings: list,
+                            content: Markup, classes: list = None) -> Markup:
+        """Used in loop to generate a table row
 
-        return widget
+        :param field_name: Name of the current table row data.
+        :param display_settings: Profile field display settings.
+        :param content: Row content.
+        :param classes: Classes to be applied to table row.
+        :return: Markup table row string.
+        :rtype: Markup
+        """
+        if classes is None:
+            classes = []
+
+        if field_name.lower() not in display_settings:
+            classes.append("visually-hidden")
+
+        row_classes = " ".join(classes)
+        return self._table_row % {"row_class": row_classes, "row_content": content}
