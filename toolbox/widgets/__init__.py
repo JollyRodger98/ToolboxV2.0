bf284eb792b11f30ec8bcbfad26b49e7e6523fcc
Tim Waldesbühl <twaldesbuehl@gmail.com>
2021-05-16 15:19:36 +0200

    Added new module files to imports

--- toolbox/widgets/__init__.py
+++ toolbox/widgets/__init__.py
@@ -1,4 +1,5 @@
 from .dashboard import HardwareWidget
 from .profile import get_default_os_profile
+from .api import WidgetAPI
 
-__all__ = ['HardwareWidget', "get_default_os_profile"]
+__all__ = ['HardwareWidget', "get_default_os_profile", "WidgetAPI"]
