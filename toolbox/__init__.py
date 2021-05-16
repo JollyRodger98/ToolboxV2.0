b9613b1b70759880727d4f69918fb159da5b589f
Tim Waldesbühl <twaldesbuehl@gmail.com>
2021-05-16 15:20:13 +0200

    New syntax for with Flask 2.0 and Jinja2 3.0

--- toolbox/__init__.py
+++ toolbox/__init__.py
@@ -3,9 +3,9 @@ from flask_mongoengine import MongoEngine
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
-app.jinja_options["extensions"].append("jinja2_time.TimeExtension")
-app.jinja_env.trim_blocks = True
-app.jinja_env.lstrip_blocks = True
+app.jinja_env.add_extension("jinja2_time.TimeExtension")
+# app.jinja_env.trim_blocks = True
+# app.jinja_env.lstrip_blocks = True
 
 DB = MongoEngine(app)
 
