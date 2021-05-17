7d7cc6f0b498ca6697de12ba5e037ef20af6ffd9
Tim Waldesbühl <twaldesbuehl@gmail.com>
2021-05-17 16:36:19 +0200

    add logging filter, new route files

--- toolbox/__init__.py
+++ toolbox/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_mongoengine import MongoEngine
+from flask.logging import logging
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
@@ -9,4 +10,7 @@ app.jinja_env.lstrip_blocks = True
 
 DB = MongoEngine(app)
 
-from toolbox import views
+import toolbox.endpoints
+from toolbox.models import LiveDataFilter
+
+logging.getLogger("werkzeug").addFilter(LiveDataFilter())
