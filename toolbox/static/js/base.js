d0af865c863b9a8e806ca3a536ad2fb1c465f02d
Tim Waldesbühl <twaldesbuehl@gmail.com>
2021-05-17 16:37:30 +0200

    Initialize tooltips on each page

--- toolbox/static/js/base.js
+++ toolbox/static/js/base.js
@@ -6,4 +6,9 @@ $(function() {
         $('#sidebar-icon').toggleClass('invisible')
         //$('#sidebar-icon').toggleClass('bi-x bi-list')
     })
+
+    let tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    let tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
 });
