4311600222834dd515dc07562efb403c1cb8ea02
Tim Waldesbühl <twaldesbuehl@gmail.com>
2021-05-17 16:34:36 +0200

    Log filter module files

--- toolbox/models/__init__.py
+++ toolbox/models/__init__.py
@@ -1,5 +1,6 @@
 from .flaskform import DisplaySettingsForm
 from .mongodb import DashboardProfiles
 from .util import widget_list
+from .logging import LiveDataFilter
 
-__all__ = ["DashboardProfiles", "DisplaySettingsForm", "widget_list"]
+__all__ = ["DashboardProfiles", "DisplaySettingsForm", "widget_list", "LiveDataFilter"]
