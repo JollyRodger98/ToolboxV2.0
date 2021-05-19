c824da1f41336d3cf3f3223698eb09e6e5d2903c
Tim Waldesbühl <twaldesbuehl@gmail.com>
2021-05-19 22:37:49 +0200

    Refactored button code, added toggle all button

--- toolbox/static/js/dashboard/live-data.js
+++ toolbox/static/js/dashboard/live-data.js
@@ -1,62 +1,61 @@
-sessionStorage.setItem("CPUInterval", "1")
-sessionStorage.setItem("MemoryInterval", "1")
-sessionStorage.setItem("CPUByCoreInterval", "1")
+if(sessionStorage.getItem("interval_seconds") == null) {sessionStorage.setItem("interval_seconds", "10")}
 $(function () {
-    /**@type {boolean}*/
-    let CPULiveToggle, MemoryLiveToggle, CPUByCoreLiveToggle
-    /**@type {number}*/
-    let CPUIntervalID, MemoryIntervalID, CPUByCoreIntervalID
-    CPULiveToggle = MemoryLiveToggle = CPUByCoreLiveToggle = false
+    let interval_id = {"cpu-percent": "", "memory-percent": "", "cpu-by-core-percent": ""}
+    let toggle_states = {"cpu-percent": false, "memory-percent": false, "cpu-by-core-percent": false}
+    let interval_seconds = sessionStorage.getItem("interval_seconds")
+    let interval_functions = {"cpu-percent": RefreshCPUTotal, "memory-percent": RefreshMemoryTotal, "cpu-by-core-percent": RefreshCPUByCore}
+    let interval_id_list = []// List to save all Interval IDs in case of double/overlapping intervals
 
-    $("button.live-data-time-btn").on("click", function () {
-        let $toggle = $(this).parents("div.btn-group").children("button.live-data-toggle")
-        let $select = $(this).parents("form").children().find("select.live-data-time-select")
-        if ($toggle.prop("id") === "cpu-percent"){
-            console.log("CPU")
-            sessionStorage.setItem("CPUInterval", String($select.prop("value")))
-        }else if ($toggle.prop("id") === "memory-percent"){
-            console.log("Memory")
-            sessionStorage.setItem("MemoryInterval", String($select.prop("value")))
-        }else if ($toggle.prop("id") === "cpu-by-core-percent"){
-            console.log("CPU by Core")
-            sessionStorage.setItem("CPUByCoreInterval", String($select.prop("value")))
+    //Select form handling and event trigger
+    $("select.live-data-time-select").on("change", function () {
+        interval_seconds = $(this).prop("value")
+        sessionStorage.setItem("interval_seconds", $(this).prop("value"))
+    }).prop("value", interval_seconds)
+
+    // Toggle button events
+    $("button.live-data-toggle").on("click", function () {
+        let button_id =  $(this).attr("id")
+        let button = $(this)
+        if (toggle_states[button_id]){
+            toggle_states[button_id] = false
+            clearInterval(interval_id[button_id])
+        }else if (!toggle_states[button_id]){
+            interval_id[button_id] = setInterval(function (){interval_functions[button_id]()}, Number(interval_seconds)*1000)
+            interval_id_list.push(interval_id[button_id])
+            toggle_states[button_id] = true
         }
+        ToggleButtonStyle(button, toggle_states[button_id])
     })
-    $("#cpu-percent").on("mouseup", function () {
-        if (CPULiveToggle) {
-            CPULiveToggle = false
-            resetToggle(CPUIntervalID, $(this))
-        }else if (!CPULiveToggle){
-            CPUIntervalID = setInterval(function (){RefreshCPUTotal()}, Number(sessionStorage.getItem("CPUInterval"))*1000)
-            CPULiveToggle = true
-            $(this).children().removeClass("text-danger").addClass("text-success")
+
+    //Event handler for toggling all buttons
+    $("#toggle-all").on("click", function () {
+        //$("button.live-data-toggle").trigger("click")
+        let icon = $(this).children("i")
+        if ((icon.attr("class")).indexOf("bi-toggle-off") >= 0){
+            icon.removeClass("bi-toggle-off").addClass("bi-toggle-on")
+            for (const toggleStatesKey in toggle_states) {
+                toggle_states[toggleStatesKey] = false
+            }
+        }else {
+            icon.addClass("bi-toggle-off").removeClass("bi-toggle-on")
+            for (const toggleStatesKey in toggle_states) {
+                toggle_states[toggleStatesKey] = true
+            }
+            //Clears all remaining Intervals and clear list
+            for (const intervalIdListKey in interval_id_list) {
+                clearInterval(interval_id_list[intervalIdListKey])
+            }
+            interval_id_list = []
         }
+        $("button.live-data-toggle").trigger("click")
     })
-    $("#memory-percent").on("mouseup", function () {
-        if (MemoryLiveToggle) {
-            MemoryLiveToggle = false
-            resetToggle(MemoryIntervalID, $(this))
-        }else if (!MemoryLiveToggle){
-            MemoryIntervalID = setInterval(function (){RefreshMemoryTotal()}, Number(sessionStorage.getItem("MemoryInterval"))*1000)
-            MemoryLiveToggle = true
-            $(this).children().removeClass("text-danger").addClass("text-success")
-        }
-    })
-    $("#cpu-by-core-percent").on("mouseup", function () {
-        if (CPUByCoreLiveToggle) {
-            CPUByCoreLiveToggle = false
-            resetToggle(CPUByCoreIntervalID, $(this))
-        }else if (!CPUByCoreLiveToggle){
-            CPUByCoreIntervalID = setInterval(function (){RefreshCPUByCore()}, Number(sessionStorage.getItem("CPUByCoreInterval"))*1000)
-            CPUByCoreLiveToggle = true
-            $(this).children().removeClass("text-danger").addClass("text-success")
-        }
-    })
+
+
 })
 
 /**Set new value for targeted percentage bar.
- * @param {number} percent_value - CPU percentage value as integer.
- * @param {string} field_name - Content of &lt;th> field belonging to percentage bar.
+ * @param {number} percent_value CPU percentage value as integer.
+ * @param {string} field_name Content of &lt;th> field belonging to percentage bar.
  * @return {undefined}
  */
 function SetPercentageBar(percent_value, field_name){
@@ -67,6 +66,11 @@ function SetPercentageBar(percent_value, field_name){
     $elem.attr("aria-valuenow", percent).attr("data-bs-original-title", percent_str)
         .css("width", percent_str).text(percent_str)
 }
+
+/**Calls API for CPU stats.
+ *
+ * @return {undefined}
+ */
 function RefreshCPUTotal() {
     let call =  $.get({url: "/tb-api?prop=cpu-percent", dataType: "json"})
     call.done(function (data, success) {
@@ -74,6 +78,11 @@ function RefreshCPUTotal() {
         SetPercentageBar(percentage, "CPU Usage Total")
     })
 }
+
+/**Calls API for Memory stats.
+ *
+ * @return {undefined}
+ */
 function RefreshMemoryTotal() {
     let call =  $.get({url: "/tb-api?prop=memory-percent", dataType: "json"})
     call.done(function (data, success) {
@@ -81,6 +90,11 @@ function RefreshMemoryTotal() {
         SetPercentageBar(percentage, "Percent")
     })
 }
+
+/**Calls API for CPU stats by core.
+ *
+ * @return {undefined}
+ */
 function RefreshCPUByCore(){
     let call =  $.get({url: "/tb-api?prop=cpu-by-core-percent", dataType: "json"})
     call.done(function (data, status) {
@@ -89,7 +103,19 @@ function RefreshCPUByCore(){
         )
     })
 }
-function resetToggle(id, button){
-    clearInterval(id)
-    button.children().addClass("text-danger").removeClass("text-success")
+
+/**Change CSS classes for toggle buttons.
+ *
+ * @param button {JQuery<HTMLElement>} Button object
+ * @param state {boolean} To which state the button will be toggled.
+ * @return {undefined}
+ */
+function ToggleButtonStyle(button, state){
+    if(state){
+        button.children("i.bi-circle-fill").addClass("visually-hidden")
+        button.children("div[role='status']").removeClass("visually-hidden")
+    }else if (!state){
+        button.children("i.bi-circle-fill").removeClass("visually-hidden")
+        button.children("div[role='status']").addClass("visually-hidden")
+    }
 }
