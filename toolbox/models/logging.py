fdc8d495ffb4c0ef8cf7fb9c22769b2ba1b5dc9f
Tim Waldesbühl <twaldesbuehl@gmail.com>
2021-05-24 22:56:21 +0200

    Filter new APIs live data requests from log

--- toolbox/models/logging.py
+++ toolbox/models/logging.py
@@ -4,7 +4,7 @@ from logging import LogRecord
 
 class LiveDataFilter(logging.Filter):
     def filter(self, record: LogRecord) -> bool:
-        filter_list = ["cpu-percent", "memory-percent", "cpu-by-core-percent"]
+        filter_list = ["cpu-percent", "memory-percent", "cpu-by-core-percent", "gpu-memory-percent", "gpu-temperature"]
         log_msg: bool = all([i not in record.getMessage() for i in filter_list])
         return log_msg
 
