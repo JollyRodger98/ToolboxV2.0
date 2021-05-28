5275317f05c1568fdc1884da0c514b05f3cc483d
JollyRodger98 <twaldesbuehl@gmail.com>
2021-05-28 14:11:40 +0200

    MacOS CLI parsing to dict finished.

--- toolbox/hardware/_re_patterns.py
+++ toolbox/hardware/_re_patterns.py
@@ -1,15 +1,19 @@
 import re
+from typing import Dict
 
 _re_netconf_name = re.compile(r"(?m)^InterfaceAlias\s*:\s((?:\w|[ *-])+\d)\r$")
 
-mac_os = {
-    "int_status": re.compile(r"^\tstatus: (active|inactive)$"),
+mac_os: Dict = {
+    "int_status": re.compile(r"(?m)^\tstatus: (active|inactive)$"),
     "int_type": re.compile(r"(?m)^\ttype: ((?:\w*[ -]?)*)$"),
     "int_ether": re.compile(r"(?m)^\tether ([a-f0-9]{2}:(?:[a-f0-9]{2}:){4}[a-f0-9]{2}) $"),
     "int_addresses": re.compile(r"(?m)^\tinet (\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) netmask ([\w|\d]{10})(?: broadcast )?(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})?.*$") ,
     "int_up_down": re.compile(r"(?m)^\t((?:down|up)link) rate: (\d{1,3}\.\d{2} Mbps) \[eff\] / (\d{1,3}\.\d{2} Mbps)(?: \[max\])?$"),
-    "int_rate": re.compile(r"(?m)^\tlink rate: (\d{1,3}\.\d{2} Mbps)$"),
-    "int_quality": re.compile(r"(?m)^\t(link quality: )(\d{1,3} \(\w*\))$"),
+    "int_rate": re.compile(r"(?m)^\tlink rate: (\d{1,3}\.\d{2} [GM]bps)$"),
+    "int_quality": re.compile(r"(?m)^\tlink quality: (\d{1,3} \(\w*\))$"),
+    "int_gateway": "(?m)^(?:\d{{1,3}}\.\d{{1,3}}\.\d{{1,3}}\.\d{{1,3}}(?:/\d\d)?|default)\s*(\d{{1,3}}\.\d{{1,3}}\.\d{{1,3}}\.\d{{1,3}})\s*[UHGSCLWImircg]*\s*({interface})\s*$",
+    "int_media": re.compile(r"(?m)^\tmedia: (autoselect)?(?: ?\(?<unknown type>\)?| \(?(?:(\d{2,4})\w*)? ?<(full|half)-duplex>\)?)$"),
+    "int_ipv6": re.compile(r"(?m)^\tinet6 ([a-f\d:]*)%[\d\w]* prefixlen \d{1,2} scopeid 0x[a-f\d]* $")
 }
 
 windows = {
--- toolbox/hardware/ipconfig.py
+++ toolbox/hardware/ipconfig.py
@@ -2,30 +2,156 @@ import ipaddress
 import random
 import re
 import subprocess
+from typing import Dict
 from collections import OrderedDict
 from re import Match
 from subprocess import CompletedProcess
-from ._re_patterns import windows, _re_netconf_name
+from ._re_patterns import windows, mac_os, _re_netconf_name
 
 
-class Terminal:
-    _re_ifconfig_status = re.compile(r"(?m)^\tstatus: (active|inactive)$")
-    _re_ifconfig_type = re.compile(r"(?m)^\ttype: ((?:\w*[ -]?)*)$")
-    _re_ifconfig_ether = re.compile(r"(?m)^\tether ([a-f0-9]{2}:(?:[a-f0-9]{2}:){4}[a-f0-9]{2}) $")
-    _re_ifconfig_addresses = re.compile(r"(?m)^\tinet (\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})"
-                                        r" netmask ([\w|\d]{10})"
-                                        r"(?: broadcast )?(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})?"
-                                        r".*$")
-    _re_ifconfig_up_down = re.compile(r"(?m)^\t((?:down|up)link) rate: "
-                                      r"(\d{1,3}\.\d{2} Mbps) \[eff\] / (\d{1,3}\.\d{2} Mbps)(?: \[max\])?$")
-    _re_ifconfig_rate = re.compile(r"(?m)^\tlink rate: (\d{1,3}\.\d{2} Mbps)$")
-    _re_ifconfig_quality = re.compile(r"(?m)^\t(link quality: )(\d{1,3} \(\w*\))$")
+class _MacOSParser:
+    _re_ifconfig_status = mac_os["int_status"]
+    _re_ifconfig_type = mac_os["int_type"]
+    _re_ifconfig_ether = mac_os["int_ether"]
+    _re_ifconfig_addresses = mac_os["int_addresses"]
+    _re_ifconfig_up_down = mac_os["int_up_down"]
+    _re_ifconfig_rate = mac_os["int_rate"]
+    _re_ifconfig_quality = mac_os["int_quality"]
+    _re_ifconfig_gateway = mac_os["int_gateway"]
+    _re_ifconfig_media = mac_os["int_media"]
+    _re_ifconfig_ipv6 = mac_os["int_ipv6"]
 
-    _re_netconf_name = _re_netconf_name
-    _re_ipconfig_name = windows["ipconfig"]["int_name"]
-    network_conf_dict_default = OrderedDict({"address": "", "netmask": "", "broadcast": "", "status": "", "type": "",
-                                             "mac": "", "rate": "", "uplink rate max": "", "uplink rate eff": "",
-                                             "downlink rate max": "", "downlink rate eff": "", "quality": ""})
+    def ifconfig_int_quality(self, interface_cli_output: str) -> Dict:
+        re_quality: Match = re.search(self._re_ifconfig_quality, interface_cli_output)
+        quality_dict: Dict = {"quality": ""}
+        if re_quality:
+            int_quality = re_quality.group(1)
+            quality_dict = {"quality": int_quality}
+        return quality_dict
+
+    def ifconfig_int_up_down_rate(self, interface_cli_output: str) -> Dict:
+        re_up_down: list = re.findall(self._re_ifconfig_up_down, interface_cli_output)
+        int_rate: dict = {
+            "rate": "", "uplink rate max": "", "uplink rate eff": "",
+            "downlink rate max": "", "downlink rate eff": ""
+        }
+
+        if re_up_down:
+            int_rate["uplink rate eff"] = re_up_down[0][1]
+            int_rate["uplink rate max"] = re_up_down[0][2]
+            int_rate["downlink rate eff"] = re_up_down[1][1]
+            int_rate["downlink rate max"] = re_up_down[1][2]
+        elif not re_up_down:
+            re_rate: Match = re.search(self._re_ifconfig_rate, interface_cli_output)
+            if re_rate:
+                int_rate["rate"] = re_rate.group(1)
+        return int_rate
+
+    def ifconfig_int_ether(self, interface_cli_output: str) -> Dict:
+        re_ether: Match = re.search(self._re_ifconfig_ether, interface_cli_output)
+        ether_dict: Dict = {"mac": ""}
+        if re_ether:
+            int_ether = re_ether.group(1)
+            ether_dict["mac"] = int_ether
+        return ether_dict
+
+    def ifconfig_int_type(self, interface_cli_output: str) -> Dict:
+        re_type: Match = re.search(self._re_ifconfig_type, interface_cli_output)
+        type_dict: Dict = {"type": ""}
+        if re_type:
+            int_type = re_type.group(1)
+            type_dict["type"] = int_type
+        elif interface_cli_output.startswith("utun"):
+            type_dict["type"] = "Virtual P2P Interface (Back to My Mac)"
+        return type_dict
+
+    def ifconfig_int_status(self, interface_cli_output: str) -> Dict:
+        re_status: Match = re.search(self._re_ifconfig_status, interface_cli_output)
+        status_dict: Dict = {"status": ""}
+        if re_status:
+            int_status = re_status.group(1)
+            status_dict["status"] = int_status
+        return status_dict
+
+    def ifconfig_int_addr(self, interface_cli_output: str) -> OrderedDict:
+        re_addresses: Match = re.search(self._re_ifconfig_addresses, interface_cli_output)
+        keys: list = ["ipv4", "netmask", "broadcast"]
+        addresses_dict: OrderedDict = OrderedDict({"ipv4": "", "netmask": "", "broadcast": ""})
+        if re_addresses:
+            interface_addresses = list(re_addresses.groups())
+            hex_netmask = interface_addresses[1][2:]
+            if all(char.isalnum() for char in hex_netmask):
+                hex_netmask = int(interface_addresses[1][2:], 16)
+                interface_addresses[1] = ipaddress.IPv4Address(hex_netmask).compressed
+            for index, address in enumerate(interface_addresses):
+                addresses_dict.update({keys[index]: address})
+        return addresses_dict
+
+    def ifconfig_int_gateway(self, routing_table_cli_output: str, interface: str) -> Dict:
+        re_pat = re.compile(self._re_ifconfig_gateway.format(interface=interface))
+        re_gateway: Match = re.search(re_pat, routing_table_cli_output)
+        gateway_dict: Dict = {"gateway": ""}
+        if re_gateway:
+            int_gateway = re_gateway.group(1)
+            gateway_dict["gateway"] = int_gateway
+        return gateway_dict
+
+    def ifconfig_int_media(self, interface_cli_output: str) -> Dict:
+        re_media: Match = re.search(self._re_ifconfig_media, interface_cli_output)
+        media_dict: Dict = {"media": "", "speed": "", "duplex": ""}
+        if re_media:
+            if re_media.group(1) is not None:
+                media_dict["media"] = re_media.group(1)
+            if re_media.group(2) is not None:
+                media_dict["speed"] = re_media.group(2)
+            if re_media.group(3) is not None:
+                media_dict["duplex"] = re_media.group(3)
+        return media_dict
+
+    def ifconfig_int_ipv6(self, interface_cli_output: str) -> Dict:
+        re_ipv6: Match = re.search(self._re_ifconfig_ipv6, interface_cli_output)
+        ipv6_dict: Dict = {"ipv6": ""}
+        if re_ipv6:
+            int_ipv6 = re_ipv6.group(1)
+            ipv6_dict["ipv6"] = int_ipv6
+        return ipv6_dict
+
+
+class _WindowsParser:
+    re_ipconfig_name = windows["ipconfig"]["int_name"]
+
+    def ipconfig_int_name(self, interface_cli_output: str) -> str:
+        re_name: Match = re.search(self.re_ipconfig_name, interface_cli_output)
+        int_name: str
+        if re_name:
+            int_name = re_name.group(1)
+        elif interface_cli_output == "Windows IP Configuration":
+            int_name = interface_cli_output
+        else:
+            int_name = f"Unknown Interface {random.randint(1, 999)}"
+        return int_name
+
+
+class Terminal(_MacOSParser, _WindowsParser):
+    network_conf_dict_default = OrderedDict({
+        "type": "",
+        "status": "",
+        "mac": "",
+        "ipv4": "",
+        "ipv6": "",
+        "netmask": "",
+        "broadcast": "",
+        "gateway": "",
+        "media": "",
+        "speed": "",
+        "duplex": "",
+        "rate": "",
+        "uplink rate max": "",
+        "uplink rate eff": "",
+        "downlink rate max": "",
+        "downlink rate eff": "",
+        "quality": "",
+    })
 
     def __init__(self, os: str):
         """Parsing output of various CLIs.
@@ -36,6 +162,7 @@ class Terminal:
         """
         self._os = os
 
+    # TODO Replace this, legacy function
     def get_ipconfig(self):
         ipconfig_raw_output = subprocess.check_output(["ipconfig", "-all"])
         ipconfig_raw_output = ipconfig_raw_output.split(b"\r\n\r\n")
@@ -69,7 +196,16 @@ class Terminal:
 
         return parsed_ipconfig
 
+    # TODO Delete
     def ipconfig_ps(self):
+        def _netconf_int_name(interface_cli_output: str) -> str:
+            re_name: Match = re.search(_re_netconf_name, interface_cli_output)
+            int_name: str
+            if re_name:
+                int_name = re_name.group(1)
+            else:
+                int_name = f"Unknown Interface {random.randint(1, 999)}"
+            return int_name
         ps_cmd = ["powershell", "-command"]
         netconf_interface_list: OrderedDict = OrderedDict()
         cmd = ps_cmd + ["Get-NetIPConfiguration", "-All", "-Detailed", "-AllCompartments"]
@@ -77,7 +213,7 @@ class Terminal:
         net_conf_cmd_out = net_conf_cmd.stdout.decode().split("\r\n\r")
         net_conf_cmd_out = [interface.strip() for interface in net_conf_cmd_out if interface not in ['\n', ""]]
         for interface_data in net_conf_cmd_out:
-            interface_name = self._netconf_int_name(interface_data)
+            interface_name = _netconf_int_name(interface_data)
             netconf_interface_list.update({interface_name: {}})
             # print(f"{interface_data}\n")
             re_tmp = re.compile(r"^InterfaceAlias\s*:\s((?:\w|[ *-])+\d)\r$", re.MULTILINE)
@@ -87,7 +223,17 @@ class Terminal:
                 pass
         return netconf_interface_list
 
-    def ipconfig_cmd(self):
+    def network_config(self) -> OrderedDict:
+        """Get Network specification and configuration based on OS.
+
+        :return: Dictionary with information about local network configuration.
+        """
+        if self._os == "Darwin":
+            return self._macos_net_conf()
+        elif self._os == "Windows":
+            return self._windows_net_conf()
+
+    def _windows_net_conf(self) -> OrderedDict:
         ipconfig_interface_list: OrderedDict = OrderedDict()
         ipconfig_cmd: CompletedProcess = subprocess.run(["ipconfig", "/all"], capture_output=True)
         ipconfig_cmd_out = ipconfig_cmd.stdout.decode().split("\r\n\r\n")
@@ -95,7 +241,7 @@ class Terminal:
         # print(*ipconfig_cmd_out, sep=f"\n{'='*50}\n")
         test = {"dict_test": r"(?:Ethernet|Wireless LAN) adapter ((?:\w|[-* ])* \d):"}
         for int_name, int_data in ipconfig_cmd_out:
-            interface_name = self._ipconfig_int_name(int_name)
+            interface_name = self.ipconfig_int_name(int_name)
             ipconfig_interface_list.update({interface_name: {}})
             re_int_name: Match = re.search(test["dict_test"], int_name)
             if re_int_name:
@@ -105,115 +251,32 @@ class Terminal:
             # print(interface_name)
         return ipconfig_interface_list
 
-    def _ipconfig_int_name(self, interface_cli_output: str) -> str:
-        re_name: Match = re.search(self._re_ipconfig_name, interface_cli_output)
-        int_name: str
-        if re_name:
-            int_name = re_name.group(1)
-        elif interface_cli_output == "Windows IP Configuration":
-            int_name = interface_cli_output
-        else:
-            int_name = f"Unknown Interface {random.randint(1, 999)}"
-        return int_name
-
-    def _netconf_int_name(self, interface_cli_output: str) -> str:
-        re_name: Match = re.search(self._re_netconf_name, interface_cli_output)
-        int_name: str = str()
-        if re_name:
-            int_name = re_name.group(1)
-        else:
-            int_name = f"Unknown Interface {random.randint(1, 999)}"
-        return int_name
-
-    def network_conf(self) -> OrderedDict:
-        """Get Network specification and configuration based on OS.
-
-        :return: Dictionary with information about local network configuration.
-        """
-        if self._os == "Darwin":
-            return self._macos_net_conf()
-
     def _macos_net_conf(self) -> OrderedDict:
-        ifconfig_interface_list = OrderedDict()
+        ifconfig_interface_list: OrderedDict = OrderedDict()
+        routing_table_cmd: CompletedProcess = subprocess.run(["netstat", "-rn"], capture_output=True)
         interface_list_cmd: CompletedProcess = subprocess.run(["ifconfig", "-l"], capture_output=True)
+        routing_table_cmd_out: str = routing_table_cmd.stdout.decode()
         interface_list_cmd_out: list = interface_list_cmd.stdout.decode().split()
         for interface in interface_list_cmd_out:
-            ifconfig_interface_list.update({interface: OrderedDict()})
+            ifconfig_interface_list.update({interface: self.network_conf_dict_default.copy()})
             interface_cmd: CompletedProcess = subprocess.run(["ifconfig", "-v", interface], capture_output=True)
             interface_cmd_out: str = interface_cmd.stdout.decode()
-            ifconfig_interface_list[interface].update(self._ifconfig_int_type(interface_cmd_out))
-            ifconfig_interface_list[interface].update(self._ifconfig_int_addr(interface_cmd_out))
-            ifconfig_interface_list[interface].update(self._ifconfig_int_status(interface_cmd_out))
-            ifconfig_interface_list[interface].update(self._ifconfig_int_ether(interface_cmd_out))
-            ifconfig_interface_list[interface].update(self._ifconfig_int_up_down(interface_cmd_out))
-            ifconfig_interface_list[interface].update(self._ifconfig_int_quality(interface_cmd_out))
-
+            ifconfig_interface_list[interface].update(self.ifconfig_int_type(interface_cmd_out))
+            ifconfig_interface_list[interface].update(self.ifconfig_int_status(interface_cmd_out))
+            ifconfig_interface_list[interface].update(self.ifconfig_int_ether(interface_cmd_out))
+            ifconfig_interface_list[interface].update(self.ifconfig_int_quality(interface_cmd_out))
+            ifconfig_interface_list[interface].update(self.ifconfig_int_up_down_rate(interface_cmd_out))
+            ifconfig_interface_list[interface].update(self.ifconfig_int_addr(interface_cmd_out))
+            ifconfig_interface_list[interface].update(self.ifconfig_int_gateway(routing_table_cmd_out, interface))
+            ifconfig_interface_list[interface].update(self.ifconfig_int_media(interface_cmd_out))
+            ifconfig_interface_list[interface].update(self.ifconfig_int_ipv6(interface_cmd_out))
         return ifconfig_interface_list
-
-    def _ifconfig_int_quality(self, interface_cli_output: str) -> OrderedDict:
-        re_quality: Match = re.search(self._re_ifconfig_quality, interface_cli_output)
-        rate_dict: OrderedDict = OrderedDict({"quality": ""})
-        if re_quality:
-            int_quality = re_quality.group(1)
-            rate_dict["quality"] = int_quality
-        return rate_dict
-
-    def _ifconfig_int_up_down(self, interface_cli_output: str) -> OrderedDict:
-        re_up_down: list = re.findall(self._re_ifconfig_up_down, interface_cli_output)
-        rate_dict: OrderedDict = OrderedDict({"rate": "", "uplink rate max": "", "uplink rate eff": "",
-                                              "downlink rate max": "", "downlink rate eff": ""})
-        if re_up_down:
-            rate_dict["uplink rate eff"] = re_up_down[0][1]
-            rate_dict["uplink rate max"] = re_up_down[0][2]
-            rate_dict["downlink rate eff"] = re_up_down[1][1]
-            rate_dict["downlink rate max"] = re_up_down[1][2]
-        elif not re_up_down:
-            re_rate: Match = re.search(self._re_ifconfig_rate, interface_cli_output)
-            if re_rate:
-                rate_dict["rate"] = re_rate.group(1)
-        return rate_dict
-
-    def _ifconfig_int_ether(self, interface_cli_output: str) -> OrderedDict:
-        re_ether: Match = re.search(self._re_ifconfig_ether, interface_cli_output)
-        ether_dict: OrderedDict = OrderedDict({"mac": ""})
-        if re_ether:
-            int_ether = re_ether.group(1)
-            ether_dict["mac"] = int_ether
-        return ether_dict
-
-    def _ifconfig_int_type(self, interface_cli_output: str) -> OrderedDict:
-        re_type: Match = re.search(self._re_ifconfig_type, interface_cli_output)
-        type_dict: OrderedDict = OrderedDict({"type": ""})
-        if re_type:
-            int_type = re_type.group(1)
-            type_dict["type"] = int_type
-        return type_dict
-
-    def _ifconfig_int_status(self, interface_cli_output: str) -> OrderedDict:
-        re_status: Match = re.search(self._re_ifconfig_status, interface_cli_output)
-        status_dict: OrderedDict = OrderedDict({"status": ""})
-        if re_status:
-            int_status = re_status.group(1)
-            status_dict["status"] = int_status
-        return status_dict
-
-    def _ifconfig_int_addr(self, interface_cli_output: str) -> OrderedDict:
-        re_addresses: Match = re.search(self._re_ifconfig_addresses, interface_cli_output)
-        keys: list = ["address", "netmask", "broadcast"]
-        addresses_dict: OrderedDict = OrderedDict({"address": "", "netmask": "", "broadcast": ""})
-        if re_addresses:
-            interface_addresses = list(re_addresses.groups())
-            hex_netmask = interface_addresses[1][2:]
-            if all(char.isalnum() for char in hex_netmask):
-                hex_netmask = int(interface_addresses[1][2:], 16)
-                interface_addresses[1] = ipaddress.IPv4Address(hex_netmask).compressed
-            for index, address in enumerate(interface_addresses):
-                addresses_dict.update({keys[index]: address})
-        return addresses_dict
 
 
 t = Terminal("Darwin")
-output = t.ipconfig_cmd()
-for i in output.items():
-    print(i)
+output = t.network_config()
+for key, value in output.items():
+    # print(value)
+    print(f'{key:10}: {value.items()}')
     pass
+# print(*output["en4"].items(), sep="\n")
