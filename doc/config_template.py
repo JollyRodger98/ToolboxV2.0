9c2b683f6ddfe28b06b5636e6a084d6db0b87ae3
Tim Waldesbühl <twaldesbuehl@gmail.com>
2021-06-09 22:54:56 +0200

    Updated template

--- doc/config_template.py
+++ doc/config_template.py
@@ -1,6 +1,11 @@
-SECRET_KEY = ""
+SECRET_KEY = ""  # Encryption key for session cookie
+DEBUG = False
 MONGODB_SETTINGS = {
-    'db': '',
-    'host': '127.0.0.1',
-    'port': 27017
+    'db': '',  # Database Name
+    'host': '',  # Database Address or Hostname
+    'port': 27017,  # 27017 is the default port
+    'username': '',  # Database Username
+    'password': '',  # Database Password
+    'authentication_source': 'admin',  # admin is the default table for db users
 }
+DASHB_DB_COL = ""  # Database Collection name for dashboard profiles
